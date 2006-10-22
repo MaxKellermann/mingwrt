b3f8d3dff3df7672154a570e262c66b65c35216c
pedroalves <pedroalves@d7810a3d-100a-0410-8641-c3624a9c11f1>
2006-10-22 17:46:45 +0000

    * test_headers.c: Adapt to __COREDLL__.
    git-svn-id: https://cegcc.svn.sourceforge.net/svnroot/cegcc/trunk/cegcc/src/mingw@748 d7810a3d-100a-0410-8641-c3624a9c11f1

--- test_headers.c
+++ test_headers.c
@@ -1,20 +1,28 @@
 #include <_mingw.h>
 #include <assert.h>
+#ifndef __COREDLL__
 #include <conio.h>
+#endif
 #include <ctype.h>
+#ifndef __COREDLL__
 #include <dir.h>
 #include <direct.h>
 #include <dirent.h>
 #include <dos.h>
 #include <errno.h>
+#endif
 #include <excpt.h>
+#ifndef __COREDLL__
 #include <fcntl.h>
+#endif
 #include <fenv.h>
 #include <float.h>
 #include <inttypes.h>
 #include <io.h>
 #include <limits.h>
+#ifndef __COREDLL__
 #include <locale.h>
+#endif
 #include <malloc.h>
 #include <math.h>
 #include <mbctype.h>
@@ -25,7 +33,9 @@
 #include <search.h>
 #include <setjmp.h>
 #include <share.h>
+#ifndef __COREDLL__
 #include <signal.h>
+#endif
 #include <stdarg.h>
 #include <stddef.h>
 #include <stdint.h>
@@ -35,12 +45,16 @@
 #include <strings.h>
 #include <tchar.h>
 #include <time.h>
+#ifndef __COREDLL__
 #include <unistd.h>
+#endif
 #include <values.h>
 #include <wchar.h>
 #include <wctype.h>
+#ifndef __COREDLL__
 #include <sys/fcntl.h>
 #include <sys/file.h>
+#endif
 #include <sys/locking.h>
 #include <sys/param.h>
 #include <sys/stat.h>
